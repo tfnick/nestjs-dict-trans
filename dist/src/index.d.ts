d95bb356cc8c90498046128c8160638f63181a27
tfnick <tfnick.liu@gmail.com>
2025-11-10 18:18:41 +0800

    fire

--- dist/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { DictTranslateModule } from './module/dict-translate.module';
-export { Translate } from './decorators/translate.decorator';
-export { DictService } from './services/dict.service';
-export { TranslateService } from './services/translate.service';
-export { MemoryCacheService } from './cache/memory-cache.service';
-export * from './interfaces/dict.interface';
